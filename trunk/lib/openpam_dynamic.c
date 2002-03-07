10a3a9f49e4bd3acfdd2ed1734f41ace8a1b9c5e
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2002-03-07 20:22:38 +0000

    Fix a number of bugs in the module cache:
     - Don't log dlopen() failures, since they're rarely interesting;
       instead, log a failure if no module was found at all.
    
     - When loading  a versioned module, store its logical name in the
       module structure rather than its physical name, since it will be
       looked up by its logical name if it's needed again.
    
     - Initialize module->next->prev when adding a module to the cache.
    
     - Set modules to NULL when releasing the last module in the cache.
    
    Sponsored by:	DARPA, NAI Labs

--- trunk/lib/openpam_dynamic.c
+++ trunk/lib/openpam_dynamic.c
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/lib/openpam_dynamic.c#2 $
+ * $P4: //depot/projects/openpam/lib/openpam_dynamic.c#3 $
  */
 
 #include <dlfcn.h>
@@ -64,15 +64,15 @@ openpam_dynamic(const char *path)
 	if (asprintf(&vpath, "%s.%d", path, LIB_MAJ) == -1)
 		goto buf_err;
 	if ((dlh = dlopen(vpath, RTLD_NOW)) == NULL) {
-		openpam_log(PAM_LOG_ERROR, "dlopen(): %s", dlerror());
 		*strrchr(vpath, '.') = '\0';
 		if ((dlh = dlopen(vpath, RTLD_NOW)) == NULL) {
-			openpam_log(PAM_LOG_ERROR, "dlopen(): %s", dlerror());
 			free(module);
 			return (NULL);
 		}
 	}
-	module->path = vpath;
+	free(vpath);
+	if ((module->path = strdup(path)) == NULL)
+		goto buf_err;
 	module->dlh = dlh;
 	for (i = 0; i < PAM_NUM_PRIMITIVES; ++i)
 		module->func[i] = dlsym(dlh, _pam_sm_func_name[i]);
