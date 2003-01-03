89a37e16c9df3c2bec7e2bc50df187200ea82e50
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2003-01-03 03:49:21 +0000

    On second thought, use RETURNS() since environment variables are strings.

--- lib/pam_getenv.c
+++ lib/pam_getenv.c
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/lib/pam_getenv.c#9 $
+ * $P4: //depot/projects/openpam/lib/pam_getenv.c#10 $
  */
 
 #include <stdlib.h>
@@ -56,12 +56,12 @@ pam_getenv(pam_handle_t *pamh,
 
 	ENTER();
 	if (pamh == NULL)
-		RETURNP(NULL);
+		RETURNS(NULL);
 	if (name == NULL || strchr(name, '=') != NULL)
-		RETURNP(NULL);
+		RETURNS(NULL);
 	if ((i = openpam_findenv(pamh, name, strlen(name))) == -1)
-		RETURNP(NULL);
-	RETURNP(strdup(pamh->env[i]));
+		RETURNS(NULL);
+	RETURNS(strdup(pamh->env[i]));
 }
 
 /**
