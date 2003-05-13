4197c073a34d921e86997e9c0e6148c4e8776f0d
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2003-05-13 14:45:45 +0000

    Build everything (rather than just the man pages) so the release will fail if the code won't build.  Finish the release by showing the MD5 checksum of the tarball.

--- dist.sh
+++ dist.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 #
-# $P4: //depot/projects/openpam/dist.sh#5 $
+# $P4: //depot/projects/openpam/dist.sh#6 $
 #
 
 set -e
 
 release=$(date '+%Y%m%d')
 distname="openpam-${release}"
+tarball="${distname}.tar.gz"
 
 make cleandir
 make cleandir
-(cd doc/man && make)
+make depend && make
 install -d -m 0755 "${distname}"
 grep -v '^#' MANIFEST | while read file; do
-    install -d -m 0755 "${distname}/$(dirname ${file})"
-    install -c -m 0644 "${file}" "${distname}/${file}"
+    install -d -m 0755 "${distname}/$(dirname ${file})" || exit 1
+    install -c -m 0644 "${file}" "${distname}/${file}" || exit 1
 done
-tar zcf "${distname}.tar.gz" "${distname}"
+tar zcf "${tarball}" "${distname}"
 rm -rf "${distname}"
 make cleandir
+make cleandir
+
+echo
+md5 "${tarball}"
