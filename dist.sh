2c97e9354be0a0090b4b60f3b836b3a192fc375b
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2003-06-09 01:52:07 +0000

    Have dist.sh automatically set the release date. Touch all files in the distribution directory before rolling the release to avoid false negative dependency checks later.

--- dist.sh
+++ dist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $P4: //depot/projects/openpam/dist.sh#7 $
+# $P4: //depot/projects/openpam/dist.sh#8 $
 #
 
 set -e
@@ -11,17 +11,21 @@ tarball="${distname}.tar.gz"
 
 gmake clean || true
 gmake distclean || true
-./autogen.sh
-./configure --with-pam-su --with-pam-unix
+sh -e autogen.sh
+sh configure --with-pam-su --with-pam-unix
 gmake
 install -d -m 0755 "${distname}"
 grep -v '^#' MANIFEST | while read file; do
     install -d -m 0755 "${distname}/$(dirname ${file})" || exit 1
-    install -c -m "${file}" "${distname}/${file}" || exit 1
+    install -c -m 0644 "${file}" "${distname}/${file}" || exit 1
 done
 for f in autogen.sh configure depcomp install-sh ltconfig ltmain.sh ; do
     chmod a+x "${distname}/${f}"
 done
+for f in configure configure.in include/security/openpam_version.h ; do
+    perl -p -i -e "s/YYYYMMDD/${release}/g" "${distname}/${f}"
+done
+find "${distname}" | xargs touch -t "${release}0000"
 tar zcf "${tarball}" "${distname}"
 rm -rf "${distname}"
 gmake clean || true
