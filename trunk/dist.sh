e38f42694ea57da0c74bc405c0e34d516c00e1d8
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2003-06-09 01:30:40 +0000

    Update the distribution script to use configure and GNU make.

--- trunk/dist.sh
+++ trunk/dist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $P4: //depot/projects/openpam/dist.sh#6 $
+# $P4: //depot/projects/openpam/dist.sh#7 $
 #
 
 set -e
@@ -9,18 +9,23 @@ release=$(date '+%Y%m%d')
 distname="openpam-${release}"
 tarball="${distname}.tar.gz"
 
-make cleandir
-make cleandir
-make depend && make
+gmake clean || true
+gmake distclean || true
+./autogen.sh
+./configure --with-pam-su --with-pam-unix
+gmake
 install -d -m 0755 "${distname}"
 grep -v '^#' MANIFEST | while read file; do
     install -d -m 0755 "${distname}/$(dirname ${file})" || exit 1
-    install -c -m 0644 "${file}" "${distname}/${file}" || exit 1
+    install -c -m "${file}" "${distname}/${file}" || exit 1
+done
+for f in autogen.sh configure depcomp install-sh ltconfig ltmain.sh ; do
+    chmod a+x "${distname}/${f}"
 done
 tar zcf "${tarball}" "${distname}"
 rm -rf "${distname}"
-make cleandir
-make cleandir
+gmake clean || true
+gmake distclean || true
 
 echo
 md5 "${tarball}"
