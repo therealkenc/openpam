b5bc8d165e16d36da3205bf998aab7d756a63117
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2003-06-18 11:32:54 +0000

    Change '-type f' to '-not -type d' to make sure we also get symlinks and whatnot.

--- trunk/clean.sh
+++ trunk/clean.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
-# $P4: //depot/projects/openpam/clean.sh#1 $
+# $P4: //depot/projects/openpam/clean.sh#2 $
 
 tmpfile="/tmp/openpam-clean.$$"
 p4 files ... | grep -v 'delete change' |
     sed 's|^.*/openpam/||; s|#.*$||' > "${tmpfile}"
-find . -type f | cut -c 3- | while read file ; do
+find . -not -type d | cut -c 3- | while read file ; do
     grep "^${file}\$" "${tmpfile}" >/dev/null || rm -v "${file}"
 done
 find . -type d -empty -print -delete
