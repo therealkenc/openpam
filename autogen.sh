8b75316539aef6487bee56611454ce99d640289d
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2007-10-24 09:58:18 +0000

    FreeBSD issues have been addressed.

--- autogen.sh
+++ autogen.sh
@@ -3,12 +3,7 @@
 # $Id$
 #
 
-if [ -d /usr/local/gnu-autotools/bin ] ; then
-	export PATH=${PATH}:/usr/local/gnu-autotools/bin
-	FIX_BROKEN_FREEBSD_PORTS="-I /usr/local/share/aclocal"
-fi
-
-aclocal ${FIX_BROKEN_FREEBSD_PORTS}
+aclocal
 libtoolize --copy --force
 autoheader
 automake -a -c --foreign
