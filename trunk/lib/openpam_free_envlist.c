b28ccd753793d15e64f14d963bf9a807c9e163ee
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2008-02-14 18:36:22 +0000

    Update copyright, and coincidentially root out the last remaining vestige of iso-8859-1 in favor of utf-8.

--- trunk/lib/openpam_free_envlist.c
+++ trunk/lib/openpam_free_envlist.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2005 Dag-Erling Coïdan Smørgrav
+ * Copyright (c) 2005-2008 Dag-Erling SmÃ¸rgrav
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
