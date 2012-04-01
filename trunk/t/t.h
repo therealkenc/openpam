14b4c34405a7430141647fbe378dc4c8abe7c624
des <des@185d5e19-27fe-0310-9dcf-9bff6b9f3609>
2012-04-01 20:34:59 +0000

    Add a handful of unit tests for openpam_readword(3). Note that one of them currently fails...

---
+++ trunk/t/t.h
@@ -0,0 +1,52 @@
+/*-
+ * Copyright (c) 2012 Dag-Erling Smørgrav
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#ifndef T_H_INCLUDED
+#define T_H_INCLUDED
+
+struct t_test {
+	int (*func)(void);
+	const char *desc;
+};
+
+#define T_FUNC(n, d)				\
+	static int t_ ## n ## _func(void);	\
+	static const struct t_test t_ ## n =	\
+	    { t_ ## n ## _func, d };		\
+	static int t_ ## n ## _func(void)
+
+#define T(n)					\
+	&t_ ## n
+
+const struct t_test **t_prepare(int, char **);
+void t_cleanup(void);
+
+void t_verbose(const char *, ...);
+
+#endif
---
+++ trunk/t/t_main.c
@@ -0,0 +1,104 @@
+/*-
+ * Copyright (c) 2012 Dag-Erling Smørgrav
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <err.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "t.h"
+
+static int verbose;
+
+void
+t_verbose(const char *fmt, ...)
+{
+	va_list ap;
+
+	if (verbose) {
+		va_start(ap, fmt);
+		vfprintf(stderr, fmt, ap);
+		va_end(ap);
+	}
+}
+
+static void
+usage(void)
+{
+
+	fprintf(stderr, "usage: %s\n", getprogname());
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	const struct t_test **t_plan;
+	const char *desc;
+	int n, pass, fail;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "v")) != -1)
+		switch (opt) {
+		case 'v':
+			verbose = 1;
+			break;
+		default:
+			usage();
+		}
+
+	argc -= optind;
+	argv += optind;
+
+	/* prepare the test plan */
+	if ((t_plan = t_prepare(argc, argv)) == NULL)
+		errx(1, "no plan\n");
+
+	/* count the tests */
+	for (n = 0; t_plan[n] != NULL; ++n)
+		/* nothing */;
+	printf("1..%d\n", n);
+
+	/* run the tests */
+	for (n = pass = fail = 0; t_plan[n] != NULL; ++n) {
+		desc = t_plan[n]->desc ? t_plan[n]->desc : "no description";
+		if ((*t_plan[n]->func)()) {
+			printf("ok %d - %s\n", n + 1, desc);
+			++pass;
+		} else {
+			printf("not ok %d - %s\n", n + 1, desc);
+			++fail;
+		}
+	}
+
+	/* clean up and exit */
+	t_cleanup();
+	exit(fail > 0 ? 1 : 0);
+}
